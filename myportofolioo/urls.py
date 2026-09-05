c9bdbe6adabe8a5ab729093899164aa676195cf1
deepanooo <islamifxadib@gmail.com>
2026-09-05 17:18:03 +0700

    perubahan style

--- myportofolioo/urls.py
+++ myportofolioo/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from .views import landing_page
+from django.conf import settings
+from django.conf.urls.static import static
 
+from .views import landing_page
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", landing_page, name="landing_page"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATICFILES_DIRS[0],
+    )
